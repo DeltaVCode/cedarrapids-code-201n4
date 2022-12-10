f577639f3de8db5f995b989540df8f934cba2d5c
Keith Dahlby <keith@deltavcodeschool.com>
2022-12-10 09:50:27 -0600

    Add missing const to define i

--- class-10/demo/syntax-errors/app.js
+++ class-10/demo/syntax-errors/app.js
@@ -32,7 +32,7 @@ CookieStand.prototype.randRate = function(){
     this.cookieSoldArray.push(Math.floor(this.aveCookies * (Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers)));
   };
   this.cookieSoldTotal = 0;
-  for(i in this.cookieSoldArray){
+  for (const i in this.cookieSoldArray) {
     this.cookieSoldTotal += this.cookieSoldArray[i];
   }
   this.cookieSoldArray.push(this.cookieSoldTotal);
